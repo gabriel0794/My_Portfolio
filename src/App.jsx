5c39fd73849b7292e3bf112cdccc1d7dd825dd79
gabriel0794 <lacar.johngabriel1@gmail.com>
2025-07-11 11:33:16 +0800

    Fix case sensitive

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import {
   FaGithub,
   FaFacebook,
